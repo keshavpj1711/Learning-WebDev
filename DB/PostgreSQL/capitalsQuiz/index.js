1156c260471e78d33e1caf6f8630168989e5f146
keshavpj1711 <keshavpj1711@gmail.com>
2025-05-20 18:03:55 +0530

    update: created the capitals quiz, only implemented loading the db part to update quiz

--- DB/PostgreSQL/capitalsQuiz/index.js
+++ DB/PostgreSQL/capitalsQuiz/index.js
@@ -1,14 +1,50 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const app = express();
 const port = 3000;
 
+// Test data
 let quiz = [
   { country: "France", capital: "Paris" },
   { country: "United Kingdom", capital: "London" },
   { country: "United States of America", capital: "New York" },
 ];
+
+// Importing data from capitals table from flags_db in your postgresql server
+const db = new pg.Client({
+  user: 'nightfury',
+  host: 'localhost',
+  database: 'flags_db',
+  password: process.env.USER_PWD,
+  port: 5432,
+});
+
+// Checking connection
+try {
+  await db.connect();
+  console.log('Connected to flags_db');
+} catch (err) {
+  console.error('Connection error', err.stack);
+}
+
+// Queries
+db.query("SELECT * FROM capitals", (err, res) => {
+  if(err){
+    console.log("Error executing query", err.stack);
+  } else{
+    quiz = res.rows;
+    // console.log(quiz[5]);
+  }
+  db.end();
+})
+
+// Don't forget to close the connection when done:
+
 
 let totalCorrect = 0;
 
@@ -21,7 +57,7 @@ let currentQuestion = {};
 // GET home page
 app.get("/", async (req, res) => {
   totalCorrect = 0;
-  await nextQuestion();
+  await nextQuestion();  // Waiting for next question
   console.log(currentQuestion);
   res.render("index.ejs", { question: currentQuestion });
 });
