a7aaa8860219e394172637e924ff69e344fc8f6d
keshavpj1711 <keshavpj1711@gmail.com>
2025-05-22 17:45:52 +0530

    update: user records fetching achieved

--- DB/PostgreSQL/relationships/famTravelTracker/index.js
+++ DB/PostgreSQL/relationships/famTravelTracker/index.js
@@ -1,39 +1,60 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
+  user: "nightfury",
   host: "localhost",
-  database: "world",
-  password: "123456",
+  database: "famTravTracker",
+  password: process.env.USER_PWD,
   port: 5432,
 });
-db.connect();
+
+try {
+  db.connect();
+  console.log("Connected to Postgres server")
+} catch (error) {
+  console.log("Error connecting to Database")
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let currentUserId = 1;
+let countries = [];
 
 let users = [
+  // demo data this is updated once the function is called
   { id: 1, name: "Angela", color: "teal" },
   { id: 2, name: "Jack", color: "powderblue" },
 ];
 
+
+async function getUsers() {
+  const result = await db.query("SELECT * FROM users")
+  if(result.rows.length === 0){
+    throw new error("Create a user") //
+  } else {
+    users = result.rows;
+    console.log(users)
+  }
+}
+
 async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
-  let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  return countries;
 }
 app.get("/", async (req, res) => {
-  const countries = await checkVisisted();
+  await checkVisisted();
+  await getUsers();
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
@@ -65,7 +86,7 @@ app.post("/add", async (req, res) => {
     console.log(err);
   }
 });
-app.post("/user", async (req, res) => {});
+app.post("/user", async (req, res) => { });
 
 app.post("/new", async (req, res) => {
   //Hint: The RETURNING keyword can return the data that was inserted.
