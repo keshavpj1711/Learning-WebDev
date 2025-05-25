afa1f65b591782826c553fbf4d6e6b24f2fbe1dd
keshavpj1711 <keshavpj1711@gmail.com>
2025-05-25 22:22:49 +0530

    update: basic react setup

---
+++ React/jsx-basics/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen'>
+        <h1 className='text-5xl'>JSX</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
